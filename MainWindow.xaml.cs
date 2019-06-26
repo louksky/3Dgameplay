c617a0232b95e12016268bb25b73ca9e7c8fce23
louksky <37488746+louksky@users.noreply.github.com>
2019-06-26 17:47:13 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿
+
+
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+
+namespace ex3_grafics
+{
+
+    public partial class MainWindow : Window
+    {
+        class Matrix
+        {
+            float[][] mat;
+            Ppoint3DCollection[][] pp;
+            Unitt un = new Unitt();
+            Matrix()
+            {
+                mat = new float[4][];
+                for (int i = 0; i < mat.GetLength(0); i++)
+                    mat[i] = new float[4];
+
+                for (int i = 0; i < mat.GetLength(0); i++)
+                    for (int j = 0; j < mat.GetLength(1); j++)
+                        mat[i][j] = i % 2;
+                pp = new Ppoint3DCollection[4][];
+                for (int i = 0; i < mat.GetLength(0); i++)
+                    pp[i] = new Ppoint3DCollection[4];
+
+                for (int i = 0; i < mat.GetLength(0); i++)
+                    for (int j = 0; j < mat.GetLength(1); j++)
+                        pp[i][j] = new Ppoint3DCollection ("", (int)un.Ppoint3DCollection_getFrequency(pp[i],1919));
+            }
+        }
+        class Ppoint3DCollection   
+        {
+            public string name;
+            public int size;
+            
+            public Ppoint3DCollection(string n, int pi)
+            {
+                this.size = pi;
+                this.name = n;
+            }
+
+        }
+        class Unitt
+        {
+            public float Ppoint3DCollection_getFrequency(Ppoint3DCollection[] fl, int req_feq_times)//O(n)
+            {
+                for (int i = 0; i < fl.Length - 1; ++i)
+                {
+                    if (fl[i].size * 3 == req_feq_times)
+                    {
+                        ///Console.WriteLine(fl[i].name);
+                        return req_feq_times;
+                    }
+                    else if (fl[i + 1].size * 2 == req_feq_times - fl[i].size)
+                    {
+                        ///Console.WriteLine(fl[i].name + "   " + fl[i + 1].name);
+                        return req_feq_times;
+                    }
+                    else if (req_feq_times - fl[i + 1].size == 2 * fl[i].size)
+                    {
+                        ///Console.WriteLine(fl[i].name + "   " + fl[i + 1].name);
+                        return req_feq_times;
+                    }
+                }
+                return 0;
+            }
+        }
+            struct point3D
+            {
+            float x, y, z;
+            point3D(float x ,float y ,float z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        int coloe = 0;
+
+        private void grdm(object sender, MouseWheelEventArgs e)
+        {
+
+            camusernnn.Position = new Point3D((camusernnn.Position.X - e.Delta / 360D), (camusernnn.Position.Y - e.Delta / 360D), (camusernnn.Position.Z - e.Delta / 360D));
+           
+            camusermmm.Position = new Point3D((camusernnn.Position.X - e.Delta / 360D), (camusernnn.Position.Y - e.Delta / 360D), (camusernnn.Position.Z - e.Delta / 360D));
+            
+           
+            coloe++;
+            if (coloe % 3 == 1)
+            {
+                diffuseennn.Brush = randnnncolormm();
+            }
+            if (coloe % 3 == 2)
+            {
+                griddnnmm.Background = randnnncolormm();
+            }
+            if (coloe % 3 == 0)
+            {
+                diffuseemmm.Brush = randnnncolormm();
+            }
+        }
+        private Brush randnnncolormm()
+        {
+            Brush returnrandcolor = Brushes.Transparent;
+
+            Random randomobj = new Random();
+
+            Type helptype = typeof(Brushes);
+
+            PropertyInfo[] settypeprop = helptype.GetProperties();
+
+            int num = randomobj.Next(settypeprop.Length);
+            returnrandcolor = (Brush)settypeprop[num].GetValue(null, null);
+
+            return returnrandcolor;
+        }
+
+        private void cngs(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void init()
+        {
+            DirectionalLight qLight = new DirectionalLight();                                                                                                   const double pi = 1.4142135623730950488016887242097;
+            Point3DCollection positionspoitnr = new Point3DCollection();                                                                                         const double pi_2minus = 2.4494897427831780981972840747059;
+            Int32Collection tringalesofshape = new Int32Collection();
+
+            //init
+            tringalesofshape.Add(0);
+            tringalesofshape.Add(3);
+            tringalesofshape.Add(2);
+            tringalesofshape.Add(0);
+            tringalesofshape.Add(2);
+            tringalesofshape.Add(1);
+            tringalesofshape.Add(1);
+            tringalesofshape.Add(2);
+            tringalesofshape.Add(3);
+            tringalesofshape.Add(0);
+            tringalesofshape.Add(1);
+            tringalesofshape.Add(3);
+            positionspoitnr.Add(new Point3D(-pi / 3.0, -1.0 / 3.0, pi_2minus / 3.0));
+            positionspoitnr.Add(new Point3D(-pi / 3.0, -1.0 / 3.0, -pi_2minus / 3.0));
+            positionspoitnr.Add(new Point3D(0.0, 1.0, 0.0));
+            positionspoitnr.Add(new Point3D(2.0 * pi / 3.0, -1.0 / 3.0, 0.0));
+
+
+
+            MeshGeometry3D qMesh = new MeshGeometry3D();
+            tr.Positions = positionspoitnr;
+            tr.TriangleIndices = tringalesofshape;
+
+           
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RotateTransform3D qRotation = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), 1));
+            DoubleAnimation qAnimation = new DoubleAnimation();
+            qAnimation.From = 1;
+            qAnimation.To = 361;
+            qAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(5000));
+            qAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            qRotation.Rotation.BeginAnimation(AxisAngleRotation3D.AngleProperty, qAnimation);
+            wtfrmodel.Transform = qRotation;
+        }
+
+        private void Button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            RotateTransform3D qRotation = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), 1));
+            DoubleAnimation qAnimation = new DoubleAnimation();
+            qAnimation.From = 1;
+            qAnimation.To = 361;
+            qAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(5000));
+            qAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            qRotation.Rotation.BeginAnimation(AxisAngleRotation3D.AngleProperty, qAnimation);
+           
+            cangroupname.Children.Add(qRotation);
+        }
+
+        private void Button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+
+
+
+
+            Application.Current.Shutdown();
+        }
+    }
+
+}
